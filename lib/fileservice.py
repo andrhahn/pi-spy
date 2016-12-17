2030e70dcdf9b0bc518d730c57ada5aa62da65fc
Andy <andrhahn@hotmail.com>
2016-12-17 12:29:35 -0600

    cleanup

--- lib/fileservice.py
+++ lib/fileservice.py
@@ -1,12 +1,7 @@
-# import boto
 import boto3
-
-AWS_ACCESS_KEY_ID = ''
-AWS_SECRET_ACCESS_KEY = ''
 
 def uploadFile(fileName):
     s3 = boto3.resource('s3')
 
     data = open(fileName, 'rb')
     s3.Bucket('pi-spy').put_object(ACL='public-read', ContentType='image/jpeg', Key='images/' + fileName, Body=data)
-
