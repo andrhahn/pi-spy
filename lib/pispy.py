720966a9263e2d35305a6bda6c9c13f72db568d9
Andy <andrhahn@hotmail.com>
2016-12-17 19:30:40 -0600

    test

--- lib/pispy.py
+++ lib/pispy.py
@@ -45,34 +45,37 @@ with picamera.PiCamera() as camera:
     camera.start_recording('/dev/null', format='h264', motion_output=MyMotionDetector(camera))
 
     while True:
-        while not motion_detected:
+        if motion_detected:
+            # while not motion_detected:
+            #     camera.wait_recording(1)
+
+            print 'motion detected...'
+
+            # camera.stop_recording()
+
+            # noinspection PyRedeclaration
+            last_capture_time = dt.datetime.now()
+
+            stream = io.BytesIO()
+
+            camera.split_recording(stream)
+
+            # camera.start_recording(stream, format='h264', quality=20)
+            camera.wait_recording(5)
+            camera.stop_recording()
+
+            print 'video captured...'
+
+            stream.seek(0)
+
+            fileName = last_capture_time.strftime('%Y-%m-%dT%H.%M.%S') + '.h264'
+
+            fileservice.uploadFile(fileName, stream, 'video/h264')
+
+            #messageservice.sendMessage('Motion detected!', 'http://s3.amazonaws.com/pi-spy/' + fileName)
+
+            camera.start_recording('/dev/null', format='h264', motion_output=MyMotionDetector(camera))
+
+            motion_detected = False
+        else:
             camera.wait_recording(1)
-
-        print 'motion detected...'
-
-        # camera.stop_recording()
-
-        # noinspection PyRedeclaration
-        last_capture_time = dt.datetime.now()
-
-        stream = io.BytesIO()
-
-        camera.split_recording(stream)
-
-        # camera.start_recording(stream, format='h264', quality=20)
-        camera.wait_recording(5)
-        camera.stop_recording()
-
-        print 'video captured...'
-
-        stream.seek(0)
-
-        fileName = last_capture_time.strftime('%Y-%m-%dT%H.%M.%S') + '.h264'
-
-        fileservice.uploadFile(fileName, stream, 'video/h264')
-
-        #messageservice.sendMessage('Motion detected!', 'http://s3.amazonaws.com/pi-spy/' + fileName)
-
-        camera.start_recording('/dev/null', format='h264', motion_output=MyMotionDetector(camera))
-
-        motion_detected = False
