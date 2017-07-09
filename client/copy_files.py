1561ab4b8c7623f73cfe16c32230ca96835ca1ff
Andy <andrhahn@hotmail.com>
2017-07-09 15:45:08 -0500

    test

--- client/copy_files.py
+++ client/copy_files.py
@@ -1,1 +1,9 @@
-print 'this should run every minute'
+import os
+
+print 'running copy_files script...'
+
+path = '/home/pi/pi-spy-files/videos'
+
+for filename in os.listdir(path):
+    # do your stuff
+    print 'file name: ' + filename
