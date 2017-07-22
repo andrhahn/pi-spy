418b4e44a1648ea59ecba862f64c198b9bc489eb
Andy <andrhahn@hotmail.com>
2017-07-22 09:41:39 -0500

    formatted email

--- client/s3_service.py
+++ client/s3_service.py
@@ -2,13 +2,14 @@
 
 import json
 import boto3
+import log_service
 
 def upload_file(bucket, body, key, content_type):
     client = boto3.client('s3')
 
     response = client.put_object(Body=body, Bucket=bucket, Key=key, ContentType=content_type, ACL='public-read')
 
-    print '==s3 put_object resp: ' + json.dumps(response)
+    log_service.debug(__name__, '3 put_object resp: ' + json.dumps(response))
 
 def send_email(subject, body, to_emails):
     client = boto3.client('ses')
@@ -35,4 +36,4 @@ def send_email(subject, body, to_emails):
         ReturnPath='info@scranthdaddy.com'
     )
 
-    print '==ses send_email resp: ' + json.dumps(response)
+    log_service.debug(__name__, 'ses send_email resp: ' + json.dumps(response))
