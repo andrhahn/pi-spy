660d63aa975af032539960a1cef4bb0c94ce4982
Andy <andrhahn@hotmail.com>
2018-03-07 22:34:21 -0600

    udpated provisioning

--- client/live_stream_start.sh
+++ client/live_stream_start.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-raspivid -n -vf -hf -t 0 -w 640 -h 480 -fps 25 -b 0 -o - | ffmpeg -i - -vcodec copy -an -f flv rtmp://34.197.186.122/live/my-stream
+raspivid -n -vf -hf -t 0 -w 640 -h 480 -fps 25 -b 0 -o - | ffmpeg -i - -vcodec copy -an -f flv rtmp://10.200.105.129/live/my-stream
