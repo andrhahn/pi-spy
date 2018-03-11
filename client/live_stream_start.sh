8c5955fec3b1755d93073aba278846af760022e1
Andy <andrhahn@hotmail.com>
2018-03-11 17:37:18 -0500

    updates

--- client/live_stream_start.sh
+++ client/live_stream_start.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
-raspivid -n -vf -hf -t 0 -w 640 -h 480 -fps 25 -b 0 -o - | ffmpeg -i - -vcodec copy -an -f flv rtmp://10.200.105.129/live/my-stream
+raspivid -n -vf -hf -t 0 -w 640 -h 480 -fps 25 -b 0 -o - | ffmpeg -i - -vcodec copy -an -f flv rtmp://10.200.105.198/live/my-stream
+
+# rotate 90 degrees (for pi zero)
+raspivid -n -vf -hf -rot 90 -t 0 -w 640 -h 480 -fps 25 -b 0 -o - | ffmpeg -i - -vcodec copy -an -f flv rtmp://10.200.105.198/live/my-stream
